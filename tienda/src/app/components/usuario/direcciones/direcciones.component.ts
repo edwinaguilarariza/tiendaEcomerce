ba43bc36af9195a0d0b2481c3b8793b839310dbe
edwin <edwinaguilarariza1@yahoo.com>
2021-09-22 18:17:45 -0500

    registro de direccion

--- tienda/src/app/components/usuario/direcciones/direcciones.component.ts
+++ tienda/src/app/components/usuario/direcciones/direcciones.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { GuestService } from 'src/app/services/guest.service';
 declare var $:any;
+declare var iziToast: any;
 
 @Component({
   selector: 'app-direcciones',
@@ -21,11 +23,36 @@ export class DireccionesComponent implements OnInit {
   public provincias : Array<any> = [];
   public distritos : Array<any> = [];
 
+  public regiones_arr : Array<any> = [];
+  public provincias_arr : Array<any> = [];
+  public distritos_arr : Array<any> = [];
+
   constructor(
-    private _guestService: GuestService
+    private _guestService: GuestService,
+    private _cienteService: ClienteService
   ) {
     this.token = localStorage.getItem('token'); 
     
+    this._guestService.get_Regiones().subscribe(
+      response=>{
+       this.regiones_arr = response;
+        
+      }
+    )
+
+    this._guestService.get_Provincias().subscribe(
+      response=>{
+       this.provincias_arr = response;
+        
+      }
+    )
+
+    this._guestService.get_Distritos().subscribe(
+      response=>{
+       this.distritos_arr = response;
+        
+      }
+    )
     
 
    }
@@ -100,6 +127,81 @@ export class DireccionesComponent implements OnInit {
     );
     
   }
+
+
+  registrar(registroForm: any){
+    if (registroForm.valid) {
+
+      this.regiones_arr.forEach(element =>{
+        if (parseInt(element.id) == parseInt(this.direccion.region)) {
+          this.direccion.region = element.name;
+        }
+      })
+
+      this.provincias_arr.forEach(element =>{
+        if (parseInt(element.id) == parseInt(this.direccion.provincia)) {
+          this.direccion.provincia = element.name;
+        }
+      })
+
+      this.distritos_arr.forEach(element =>{
+        if (parseInt(element.id) == parseInt(this.direccion.distrito)) {
+          this.direccion.distrito = element.name;
+        }
+      })
+
+
+
+      let data = {
+          destinatario  :this.direccion.destinatario,
+          dni  :this.direccion.dni,
+          zip  :this.direccion.zip,
+          direccion  :this.direccion.direccion,
+          telefono  :this.direccion.telefono,
+          pais  :this.direccion.pais,
+          region  :this.direccion.region,
+          provincia  :this.direccion.provincia,
+          distrito  :this.direccion.distrito,
+          principal  :this.direccion.principal,
+          cliente: localStorage.getItem('id'),
+      }
+
+      this._cienteService.registro_direccion_cliente(data,this.token).subscribe(
+        response=>{
+          this.direccion  = {
+            pais:'',
+            region: '',
+            provincia:'',
+            distrito:'',
+            principal: false
+          };
+          $('#sl-region').prop('disabled',true);
+          $('#sl-provincia').prop('disabled',true);
+          $('#sl-distrito').prop('disabled',true);
+          iziToast.show({
+            title:'SUCCESS',
+            titleColor:'#1DC74C',
+            color: '#FFF',
+            class: 'text-success',
+            position:'topRight',
+            message:'se agrego la nueva direccion correctamente'
+          })
+        }
+      )
+      console.log(data);
+      
+    } else {
+      iziToast.show({
+        title:'Error',
+        titleColor:'#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position:'topRight',
+        message:'los datos del formulario no son validos'
+      })
+    }
+  }
+
 
 }
  
